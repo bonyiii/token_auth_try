69cf5f7a54b295a7713ee158520d0682accb04d5
boni <boni@twine.hu>
2016-11-13 20:00:39 +0100

    reimplementing combineReducers by hand 16

--- frontend/counter_test.js
+++ frontend/counter_test.js
@@ -1,6 +1,6 @@
 import expect, { createSpy, spyOn, isSpy } from 'expect'
 import deepFreeze from 'deep-freeze'
-import { createStore, combineReducers } from 'redux'
+import { createStore } from 'redux'
 
 const todo = (state, action) => {
   switch(action.type) {
@@ -43,6 +43,21 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
     return action.filter
   default:
     return state
+  }
+}
+
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](
+          state[key],
+          action
+        )
+        return nextState
+      },
+      {}
+    )
   }
 }
 
