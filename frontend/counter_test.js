946c6311660a3f9ff39692985230f809f108cfc0
boni <boni@twine.hu>
2016-11-13 19:51:25 +0100

    replace todoApp native implementation with combineReducers

--- frontend/counter_test.js
+++ frontend/counter_test.js
@@ -1,6 +1,6 @@
 import expect, { createSpy, spyOn, isSpy } from 'expect'
 import deepFreeze from 'deep-freeze'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 
 const todo = (state, action) => {
   switch(action.type) {
@@ -46,18 +46,10 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
   }
 }
 
-const todoApp = (state = {}, action) => {
-  return {
-    todos: todos(
-      state.todos,
-      action
-    ),
-    visibilityFilter:visibilityFilter(
-      state.visibilityFilter,
-      action
-    )
-  }
-}
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+})
 
 const store = createStore(todoApp)
 
