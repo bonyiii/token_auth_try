eb2719f346d69b1b3e2600f40d2cef0dac4e10aa
boni <boni@twine.hu>
2016-11-26 03:52:55 +0100

    using provider from react-redux 26

--- frontend/todo.jsx
+++ frontend/todo.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom'
 import React, { PropTypes } from 'react'
 import { combineReducers } from 'redux'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import todos from './reducers/todos.js'
 import visibilityFilter from './reducers/visibility_filter.js'
@@ -11,21 +12,6 @@ const todoApp = combineReducers({
   todos,
   visibilityFilter
 })
-
-class Provider extends React.Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    }
-  }
-
-  render() {
-    return this.props.children
-  }
-}
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-}
 
 ReactDOM.render(
   <Provider store={createStore(todoApp)}>
