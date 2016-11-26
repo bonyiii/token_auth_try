d76581db13d8393fdf19ec49a124969f5c166133
boni <boni@twine.hu>
2016-11-26 02:50:12 +0100

    make add_todo a mixed presentational an container component 23

--- frontend/components/todo_app.jsx
+++ frontend/components/todo_app.jsx
@@ -3,17 +3,10 @@ import AddTodo from '../ui/add_todo.jsx'
 import VisibleTodoList from '../components/visible_todo_list.jsx'
 import Footer from '../ui/footer.jsx'
 
-let nextTodoId = 0
+
 const TodoApp = () => (
   <div>
-    <AddTodo
-        onAddTodo={ (text) =>
-          store.dispatch({
-            type: 'ADD_TODO',
-            id: nextTodoId++,
-            text
-          })}
-    />
+    <AddTodo/>
     <VisibleTodoList/>
     <Footer/>
   </div>
--- frontend/ui/add_todo.jsx
+++ frontend/ui/add_todo.jsx
@@ -1,25 +1,37 @@
+import store from '../store/store.js'
+
+let nextTodoId = 0
 const AddTodo = ({
   onAddTodo
 }) => {
   let input
+
   return (
     <div>
-      <input
-          ref={ node => {
-              input = node
-            }}
-          onKeyPress={ (e) => {
-              if (e.key === "Enter") {
-                onAddTodo(input.value)
-                input.value = ""
-              }
-            }}
-      />
-      <button onClick={ () => {
-          onAddTodo(input.value)
-          input.value = ""
-        }}>
-        Add Todo
+    <input
+    ref={ node => {
+      input = node
+    }}
+    onKeyPress={ (e) => {
+      if (e.key === "Enter") {
+        store.dispatch({
+          type: 'ADD_TODO',
+          id: nextTodoId++,
+          text: input.value
+        })
+        input.value = ""
+      }
+    }}
+    />
+    <button onClick={ () => {
+      store.dispatch({
+        type: 'ADD_TODO',
+        id: nextTodoId++,
+        text: input.value
+      })
+      input.value = ""
+    }}>
+    Add Todo
       </button>
     </div>
   )
