f3e114922f6a062f3df0241f92a3f11d8b98582a
boni <boni@twine.hu>
2016-12-07 09:51:54 +0100

    filte link connect 29

--- frontend/components/filter_link.jsx
+++ frontend/components/filter_link.jsx
@@ -1,39 +1,30 @@
 import Link from '../ui/link.jsx'
+import { connect } from 'react-redux'
 
-export default class FilterLink extends React.Component {
-  componentDidMount() {
-    const { store } = this.context
-    this.unsubscribe =  store.subscribe(() =>
-      this.forceUpdate()
-    )
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-
-  render() {
-    const props = this.props
-    const { store } = this.context
-    const state = store.getState()
-
-    return (
-      <Link
-      active= {
-        props.filter === state.visibilityFilter
-      }
-      onClick={() =>
-        store.dispatch({
-          type: 'SET_VISIBILITY_FILTER',
-          filter: props.filter
-        })
-      }
-      >
-      {props.children}
-      </Link>
-    )
+const mapStateToProps = (
+  state,
+  ownProps
+) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
   }
 }
-FilterLink.contextTypes = {
-  store: React.PropTypes.object
+const mapDispatchToProps = (
+  dispatch,
+  ownProps
+) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      })
+    }
+  }
 }
+const FilterLink = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Link)
+
+export default FilterLink
