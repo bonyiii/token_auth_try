9559c688be56a8694021d62e33d0f53c80499d3a
boni <boni@twine.hu>
2016-12-13 22:25:42 +0100

    idiomatic redux 2, root reducers extracted

--- frontend/todo.jsx
+++ frontend/todo.jsx
@@ -1,17 +1,11 @@
 import ReactDOM from 'react-dom'
 import React, { PropTypes } from 'react'
-import { combineReducers } from 'redux'
+
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import todos from './reducers/todos.js'
-import visibilityFilter from './reducers/visibility_filter.js'
 import TodoApp from './components/todo_app.jsx'
-
-const todoApp = combineReducers({
-  todos,
-  visibilityFilter
-})
+import todoApp from './reducers'
 
 ReactDOM.render(
   <Provider store={createStore(todoApp)}>
