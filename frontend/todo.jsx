d8c1b335966bd07bf7792d324c1770685421a237
boni <boni@twine.hu>
2016-12-14 09:01:28 +0100

    redux 2, save state to localstore

--- frontend/todo.jsx
+++ frontend/todo.jsx
@@ -3,21 +3,23 @@ import React, { PropTypes } from 'react'
 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import {saveState, loadState } from './lib/local_storage'
 
 import TodoApp from './components/todo_app.jsx'
 import todoApp from './reducers'
 
-const persistedState ={
-  todos: [
-    {
-      id: '0',
-      text: 'Welcome back!',
-      completed: false
-    }
-  ]
-}
+const persistedState = loadState()
 
-const store = createStore(todoApp, persistedState)
+const store = createStore(
+  todoApp,
+  persistedState
+)
+
+store.subscribe(() => {
+  saveState({
+    todos: store.getState().todos
+  })
+})
 console.log(store.getState())
 
 ReactDOM.render(
