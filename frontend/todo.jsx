f28a9c2029f8cb72f25bfed14793da145f61344a
boni <boni@twine.hu>
2016-12-14 09:09:17 +0100

    redux 2, lodash throttle

--- frontend/todo.jsx
+++ frontend/todo.jsx
@@ -4,6 +4,7 @@ import React, { PropTypes } from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import {saveState, loadState } from './lib/local_storage'
+import throttle from 'lodash/throttle'
 
 import TodoApp from './components/todo_app.jsx'
 import todoApp from './reducers'
@@ -15,12 +16,13 @@ const store = createStore(
   persistedState
 )
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState({
     todos: store.getState().todos
   })
-})
-console.log(store.getState())
+  console.log(store.getState())
+}, 1000))
+
 
 ReactDOM.render(
   <Provider store={store}>
