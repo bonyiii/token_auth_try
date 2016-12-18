4381ca54d4b8c35d3e6b3a8c63aa4b28d160e149
boni <boni@twine.hu>
2016-12-18 20:12:04 +0100

    redux 2 part 11 todos from array to object

--- frontend/reducers/todos.js
+++ frontend/reducers/todos.js
@@ -1,35 +1,54 @@
+import { combineReducers } from 'redux'
 import todo from './todo.js'
 
-const todos = (state = [], action) => {
+const byId = (state = {}, action) => {
   switch(action.type) {
-    case 'ADD_TODO':
-      return [
+  case 'ADD_TODO':
+  case 'TOGGLE_TODO':
+    return {
         ...state,
-        todo(undefined, action)
-      ]
-    case 'TOGGLE_TODO':
-      return state.map((t) => todo(t, action))
-    default:
-      return state
+      [action.id]: todo(state[action.id], action)
+    }
+  default:
+    return state
   }
 }
 
+const allIds = (state = [], action) => {
+  switch(action.type) {
+  case 'ADD_TODO':
+    return [...state, action.id]
+  default:
+    return state
+  }
+}
+
+const todos = combineReducers({
+  byId,
+  allIds
+})
+
 export default todos
+
+const getAllTodos = (state) => (
+  state.allIds.map( id => state.byId[id])
+)
 
 export const getVisibleTodos = (
   state,
   filter
 ) => {
+  const allTodos = getAllTodos(state)
   switch(filter) {
-    case 'all':
-      return state;
-    case 'completed':
-      return state.filter(
-        t => t.completed
-      )
-    case 'active':
-      return state.filter(
-        t => !t.completed
-      )
+  case 'all':
+    return allTodos;
+  case 'completed':
+    return allTodos.filter(
+      t => t.completed
+    )
+  case 'active':
+    return allTodos.filter(
+      t => !t.completed
+    )
   }
 }
