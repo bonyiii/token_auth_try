4deaff5fe7a3b5edefa4f55c6b690a0bfede90a5
boni <boni@twine.hu>
2016-12-21 17:09:48 +0100

    redux 2 part 17 middlewars with currying
    function(function returns functions,  that returns functions)

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,45 +1,50 @@
 import { createStore } from 'redux'
 import todoApp from '../reducers'
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch
+const logger = (store) => {
+  return (next) => {
+    if (!console.group) {
+      return next
+    }
 
-  if (!console.group) {
-    return rawDispatch
-  }
-
-  return (action) => {
-    console.group(action.type)
-    console.log('%c prev.state', 'color: gray', store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = rawDispatch(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
+    return (action) => {
+      console.group(action.type)
+      console.log('%c prev.state', 'color: gray', store.getState())
+      console.log('%c action', 'color: blue', action)
+      const returnValue = next(action)
+      console.log('%c next state', 'color: green', store.getState())
+      console.groupEnd(action.type)
+      return returnValue
+    }
   }
 }
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch
-
-  return (action) => {
-    if (typeof action.then === 'function' ) {
-      return action.then(rawDispatch)
+const promise = (store) => {
+  return (next) => {
+    return (action) => {
+      if (typeof action.then === 'function' ) {
+        return action.then(next)
+      }
+      return next(action)
     }
-    return rawDispatch(action)
   }
+}
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach((middleware) => (
+    store.dispatch = middleware(store)(store.dispatch)
+  ))
 }
 
 const configureStore = () => {
-  const store = createStore(
-    todoApp,
-  )
+  const store = createStore(todoApp)
+  const middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store)
+    middlewares.push(logger)
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store)
+  wrapDispatchWithMiddlewares(store, middlewares)
 
   return store
 }
