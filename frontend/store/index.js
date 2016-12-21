8c3bddf60c4ecd6ef4640e669f9b29cb61d3a8f4
boni <boni@twine.hu>
2016-12-21 17:12:21 +0100

    redux 2 part 17 currying functions shorter version

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,33 +1,27 @@
 import { createStore } from 'redux'
 import todoApp from '../reducers'
 
-const logger = (store) => {
-  return (next) => {
-    if (!console.group) {
-      return next
-    }
+const logger = (store) => (next) => {
+  if (!console.group) {
+    return next
+  }
 
-    return (action) => {
-      console.group(action.type)
-      console.log('%c prev.state', 'color: gray', store.getState())
-      console.log('%c action', 'color: blue', action)
-      const returnValue = next(action)
-      console.log('%c next state', 'color: green', store.getState())
-      console.groupEnd(action.type)
-      return returnValue
-    }
+  return (action) => {
+    console.group(action.type)
+    console.log('%c prev.state', 'color: gray', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = next(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
   }
 }
 
-const promise = (store) => {
-  return (next) => {
-    return (action) => {
-      if (typeof action.then === 'function' ) {
-        return action.then(next)
-      }
-      return next(action)
-    }
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function' ) {
+    return action.then(next)
   }
+  return next(action)
 }
 
 const wrapDispatchWithMiddlewares = (store, middlewares) => {
