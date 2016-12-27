7b06fb820a9a02eb222806ce991f123cbacd6589
boni <boni@twine.hu>
2016-12-27 23:32:18 +0100

    redux 2 part 22 thunk

--- frontend/actions/index.js
+++ frontend/actions/index.js
@@ -1,7 +1,7 @@
 import { v4 } from 'node-uuid'
 import * as api from '../lib/fake_api'
 
-export const requestTodos = (filter) => ({
+const requestTodos = (filter) => ({
   type: 'REQUEST_TODOS',
   filter
 })
@@ -23,8 +23,10 @@ const receiveTodos = (filter, response) => ({
   response
 })
 
-export const fetchTodos = (filter) => (
-  api.fetchTodos(filter).then(response => (
-    receiveTodos(filter, response)
+export const fetchTodos = (filter) => (dispatch) => {
+  dispatch(requestTodos(filter))
+
+  return api.fetchTodos(filter).then(response => (
+    dispatch(receiveTodos(filter, response))
   ))
-)
+}
